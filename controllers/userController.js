b4a2c3a4404b03bcbde8ab3842a76010258fefb5
deepakdevfile <deepakdevfile@gmail.com>
2026-08-07 17:43:27 +0530

    C6: added functionality to register new user in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,33 @@
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { User } from "../models/userModel.js";
 
 const registerUser = asyncHandler( async(req, res) => {
-    res.json({ message: "Registered user successfully"})
+    
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        throw new Error("All fields are required");
+    }
+
+    const userExits = await User.findOne({ email })
+
+    if(userExits){
+        res.status(400).redirect('/login')
+        throw new Error("User Exits")
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({ name, email, password: hashedPassword })
+
+    if(user){
+        res.status(200).json({ _id: user.id, name: user.name, email: user.email })
+    } else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 const loginUser = asyncHandler( async(req, res) => {
